803007dfe68271b298b92dfa0aa01b7bee5bf208
Raymond Ha <Haro@mail.uc.edu>
2023-11-02 21:39:52 -0400

    Finished

--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -1,5 +1,7 @@
-public class Main {
+public class TicTacToeRunner {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        TicTacToeFrame frame = new TicTacToeFrame();
     }
 }
